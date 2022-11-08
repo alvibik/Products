9d4c23c1508ca63a417dfa04854e5933ecf9773d
Александр Биктеев <bikteev@mail.ru>
2022-11-08 16:20:18 +0600

    Initial Commit

---
+++ Products/ProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsApp.swift
+//  Products
+//
+//  Created by albik on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
